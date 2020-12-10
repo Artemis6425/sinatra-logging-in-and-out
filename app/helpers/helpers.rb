e221cea5137607128b630ea852b03c3503e04abd
Artemis6425 <aurorarose6425@gmail.com>
2020-12-10 16:23:26 -0600

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+    def self.current_user(params)
+        @user = User.find_by(params[:username])
+        @user
+    end
+    def self.is_logged_in?(hash)
+        hash.include?(:user_id) ? true : false
+    end
 end
